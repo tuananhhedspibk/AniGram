e95bb56e20fc7482da3ef1d7059b386ab60fb687
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2017-02-26 22:00:12 +0700

    Fix some ajax bug and build index users view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
-	before_action :logged_in_user, only: [:edit, :update, :delete]
+	before_action :logged_in_user, only: [:edit, :update, :delete, :index]
 	before_action :correct_user, only: [:edit, :update]
 	before_action :admin_user, only: :destroy
+
+	def index
+		@users = User.all
+	end
 
 	def new
 		@user = User.new
