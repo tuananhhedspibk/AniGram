8e8cda58de8d3a00f8dd6e5f7ddc3b68e675ca55
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2017-02-26 16:30:16 +0700

    Update view, and fix follow feature

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,7 +3,7 @@ class RelationshipsController < ApplicationController
 
 	def create
 		@user = User.find(params[:followed_id])
-		current_user.follow(user)
+		current_user.follow(@user)
 		respond_to do |format|
 			format.html {redirect_to @user}
 			format.js
@@ -12,7 +12,7 @@ class RelationshipsController < ApplicationController
 
 	def destroy
 		@user = Relationship.find(params[:id]).followed
-		current_user.unfollow(user)
+		current_user.unfollow(@user)
 		respond_to do |format|
 			format.html {redirect_to @user}
 			format.js
