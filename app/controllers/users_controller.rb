4a9bdb6207203e609e49794eb8e1dc9cc828928b
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2017-03-09 23:15:48 +0700

    Create thumb version of uploaded picture and create upload ava link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,7 +60,7 @@ class UsersController < ApplicationController
 	private
 		def user_params
 			params.require(:user).permit(:name, :email, :gender, :password,
-										 :password_confirmation)
+										 :password_confirmation, :avatar)
 		end
 
 		# Before filters
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,7 +5,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 	# Include RMagick or MiniMagick support:
 	# include CarrierWave::RMagick
 	include CarrierWave::MiniMagick
-	process resize_to_limit: [150, 150]
+	process resize_to_limit: [270, 270]
 
 	# Choose what kind of storage to use for this uploader:
 	storage :file
@@ -23,16 +23,23 @@ class AvatarUploader < CarrierWave::Uploader::Base
 	end
 
 	# Process files as they are uploaded:
-	# process :scale => [200, 300]
 	#
 	# def scale(width, height)
-	#   # do something
+	#	process :scale => [200, 300]
 	# end
 
 	# Create different versions of your uploaded files:
-	# version :thumb do
-	#   process :resize_to_fit => [50, 50]
-	# end
+	version :thumb do
+		process :resize_to_fit => [150, 150]
+	end
+
+	version :thumb_small do
+		process :resize_to_fit => [90, 90]
+	end
+
+	version :thumb_super_small do
+		process :resize_to_fit => [60, 60]
+	end
 
 	# Add a white list of extensions which are allowed to be uploaded.
 	# For images you might use something like this:
@@ -45,5 +52,4 @@ class AvatarUploader < CarrierWave::Uploader::Base
 	# def filename
 	#   "something.jpg" if original_filename
 	# end
-
 end
